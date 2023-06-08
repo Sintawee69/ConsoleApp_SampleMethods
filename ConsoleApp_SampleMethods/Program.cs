e9f578a961560d8b43055d523ac4f18211576e15
Sintawee69 <sintawee.kit@sbac.ac.th>
2023-06-08 11:44:46 +0700

    Add project files.

---
+++ ConsoleApp_SampleMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SampleMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program objProg = new Program();
+
+            objProg.SampleMethod = (); //Call object method
+
+            SampleMethod(); // Call static method
+        }
+        static void SampleMethod()
+        {
+            Console.WriteLine("I'm hugry");
+            Console.ReadKey();
+        }
+    }
+}
